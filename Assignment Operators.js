1b60c4da8f617426be4544b130aff617124f2764
sreeram54 <vaddisreeram54@gmail.com>
2022-08-17 09:13:44 +0530

    Assignment Operators

---
+++ Assignment Operators.js
@@ -0,0 +1,18 @@
+
+var a=10;
+console.log(a);
+
+var x=2;
+console.log(x+=2); // or x = x + 2
+
+var a=10;
+console.log(a-=2); // or a = a - 2
+
+var x=2;
+console.log(x*=2); // or x = x * 2
+
+var a=4;
+console.log(a/=2); // or a = a / 2
+
+var x=3;
+console.log(x**=2); // or x = x ** 2
